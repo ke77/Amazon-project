1d6eef092c4a908e10a02612ac0574792b784971
Kelvin Kobby <agbakpeykelvin@gmail.com>
2024-09-12 11:58:13 +0000

    converted code to promises

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -123,3 +123,16 @@ export function updateQuantity(productId, newQuantity) {
     saveToStorage();
 }
 
+
+
+export function loadCart(func) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener('load', () => {
+        console.log(xhr.response);
+        func();
+    });
+
+    xhr.open('GET', 'https://supersimplebackend.dev/cart');
+    xhr.send();
+}
--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -2,14 +2,31 @@ import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from './checkout/checkoutHeader.js';
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 // import '../data/cart_oop.js';
 // import '../data/cart_class.js';
 // import '../data/backend_practice.js';
 
 
 
-loadProducts(() => {
+Promise.all([
+    new Promise((resolve) => {
+        loadProducts(() => {
+            resolve('value 1');
+        });
+    
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then((values) => {
+    console.log(values);
     renderCheckoutHeader();
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
